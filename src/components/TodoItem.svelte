0862070321e54db382747ddcf33c1348680112f2
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-06-12 15:08:11 +0300

    init commit

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let toDoList;
+    export let i;
+    let isEditing = false;
+
+    function setEditing(editing) {
+        isEditing = editing; 
+    }
+
+    function deleteTodo(i) {
+        toDoList.splice(i, 1);
+        toDoList = toDoList; 
+    }
+</script>
+
+<div style="display: flex; align-items: baseline; width: 700px; padding: 20px; margin: 0 auto; gap: 10px;">
+    {#if isEditing}
+        <input type="text" bind:value={toDoList[i].content}>
+    {:else}
+        <input type="checkbox" bind:checked={toDoList[i].checked}>
+        <h4 style="flex-grow: 1">{toDoList[i].content}</h4>
+    {/if}
+    <div style="display: flex; gap: 10px;">
+        {#if isEditing}
+            <button on:click={() => setEditing(false)}>Save</button>
+        {:else}
+            <button on:click={() => setEditing(true)}>Edit</button>
+        {/if}
+        <button on:click={() => deleteTodo(i)}>Delete</button>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</svelte:head>
+
+<script>
+	import TodoItem from "../components/TodoItem.svelte";
+
+    let toDoList = []; 
+    let textInput = "";
+
+    function addToDo() {
+        toDoList = [...toDoList, { content: textInput, checked: false }]
+        textInput = "";
+    }
+</script>
+
+<div style="margin: 0 auto; padding: 20px; width: 700px;">
+    <h2 style="text-align: center;">ToDo List</h2>
+    <p>Enter your ToDo here</p>
+    <div style="display: flex; gap: 10px;">
+        <input type="text" bind:value={textInput}>
+        <button style="width: 200px;" on:click={addToDo}>Add</button>
+    </div>
+</div>
+
+{#each toDoList as toDo, i}
+    <TodoItem toDoList={toDoList} i={i}/>
+{/each}
